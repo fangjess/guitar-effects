a417a8b29ccef3c64765879739f01bc11a76cc2c
fangjess <jessf.fang@gmail.com>
2026-03-16 13:58:34 -0700

    edited comb filter

--- effects/comb-filter.py
+++ effects/comb-filter.py
@@ -12,9 +12,9 @@ class CombFilter:
     def __init__(self, delay_length, g2=0.84, lowpass=0.5): # Freeverb algorithm uses g2=0.84 and lowpass=0.5
         self.buffer = np.zeros(delay_length)
         self.g2 = g2
-        self.lowpass = lowpass          # lowpass coefficient
-        self.filter_state = 0.0    # previous lowpass output to be fed back into buffer
-        self.write_pos = 0
+        self.lowpass = lowpass  # lowpass coefficient
+        self.filter_state = 0.0 # previous lowpass output to be fed back into buffer
+        self.write_pos = 0  # current location in the buffer
 
     def process(self, signal):
         
