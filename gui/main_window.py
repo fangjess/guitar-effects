87fff6d3d34854f46aa05fb0fa9b48d5521e1afb
fangjess <jessf.fang@gmail.com>
2026-05-14 23:32:35 -0700

    added tone dial

--- gui/main_window.py
+++ gui/main_window.py
@@ -21,19 +21,38 @@ class ReverbPedal(QWidget):
         self.mixDial.setMinimum(0)
         self.mixDial.setMaximum(100)
         self.mixDial.setValue(30) # because in freeverb init: mix = 0.3
+        self.mixDial.valueChanged.connect(self.onMixChanged)
         self.mixLabel = QLabel(f"Mix: {self.mixDial.value()}%")
         self.mixLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.mixDial.valueChanged.connect(self.onMixChanged)
         layout.addWidget(self.mixDial)
         layout.addWidget(self.mixLabel)
         self.setLayout(layout)
+
         # tone knob
+        self.toneDial = QDial()
+        self.toneDial.setMinimum(0)
+        self.toneDial.setMaximum(100)
+        self.toneDial.setValue(50)
+        self.toneDial.valueChanged.connect(self.onToneChanged)
+        self.toneLabel = QLabel(f"Tone: {self.toneDial.value()}%")
+        self.toneLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.toneDial)
+        layout.addWidget(self.toneDial)
+        
         # decay knob
         # on/off switch
 
+        self.setLayout(layout)
+
     def onMixChanged(self, val):
-        self.reverb.mix = val/100
+        self.reverb.mix = val / 100
         self.mixLabel.setText(f"Mix: {val}%")
+
+    def onToneChanged(self, val):
+        self.reverb.lowpass = 1.0 - (val / 100)
+        for comb in self.reverb.combs:
+            comb.lowpass =  self.reverb.lowpass
+        self.toneLabel
 
 class MainWindow(QMainWindow):
     def __init__(self, reverb):
